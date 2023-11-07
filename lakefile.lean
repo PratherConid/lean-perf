920305ce7bb80efb136f49049392f06ed1f1e2ba
IndPrinciple <syxx2013050321@163.com>
2023-11-07 18:59:07 +0800

    fix: avoid linker failure

--- lakefile.lean
+++ lakefile.lean
@@ -6,10 +6,9 @@ require Duper from git "https://github.com/leanprover-community/duper.git"@"main
 package «Perf» where
   -- add package configuration options here
 
-lean_lib «Perf» where
-  -- add library configuration options here
-
 @[default_target]
+lean_lib «Perf» where
+
 lean_exe «perf» where
   root := `Main
   -- Enables the use of the Lean interpreter by the executable (e.g.,
